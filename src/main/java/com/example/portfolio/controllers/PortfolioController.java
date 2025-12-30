9f50b5e96b32aa8bcc0de13bd6e31276bf66631f
tawadeshubham16 <tawade.shubham16@gmail.com>
2025-12-30 12:19:08 +0000

    Update PortfolioController.java

--- src/main/java/com/example/portfolio/controllers/PortfolioController.java
+++ src/main/java/com/example/portfolio/controllers/PortfolioController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PortfolioController {
 
-    @GetMapping("/portfolio")
+    @GetMapping("/")
     public String getPortfolio() {
         return "Portfolio data";
     }
